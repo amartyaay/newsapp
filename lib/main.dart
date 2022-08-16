21f2eb7d7de5f1f48beeef729a0335dc1671b414
Amartya <62711821+amartyaay@users.noreply.github.com>
2022-08-16 09:24:45 +0530

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:newsapp/firebase_options.dart';
 import 'package:newsapp/screens/home_view.dart';
+import 'package:newsapp/screens/loading_page.dart';
+import 'package:newsapp/screens/login_view.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
@@ -9,8 +14,33 @@ Future<void> main() async {
     const ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Home(),
+        home: MyApp(),
       ),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: ((context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user == null) {
+                return const LoginPage();
+              } else {
+                return const Home();
+              }
+            default:
+              return const LoadingPage();
+          }
+        }));
+  }
 }
---
+++ lib/screens/login_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class LoginPage extends ConsumerStatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () async {},
+          child: const Text('Google Sign In'),
+        ),
+      ),
+    );
+  }
+}
